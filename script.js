d84210bea30c65180514ea4ab2ee180541d6b5bd
Bara Maresova <bbmaresova@gmail.com>
2020-10-30 19:28:25 +0100

    skoro hotovo, alert vyskakuje moc brzy

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const hra = (event) => {
 
     activePlayer.src = 'obrazky/cross.svg';
     activePlayer.alt = 'Obrázek křížku';
+    vitezstvi(event.target);
     player = 'krizek';
   } else if (player === 'krizek') {
     event.target.classList.add('hraciPole--krizek');
@@ -21,10 +22,12 @@ const hra = (event) => {
 
     activePlayer.src = 'obrazky/circle.svg';
     activePlayer.alt = 'Obrázek kolečka';
+    vitezstvi(event.target);
     player = 'kolecko';
   } else null;
-  console.log(getPosition(event.target));
-  console.log(getSymbol(event.target));
+  console.log('Get position', getPosition(event.target));
+  console.log('Get symbol', getSymbol(event.target));
+  console.log('Winning', isWinningMove(event.target));
 };
 
 for (let i = 0; i < hraciPole.length; i += 1) {
@@ -54,5 +57,68 @@ const getSymbol = (pole) => {
     return 'cross';
   } else if (pole.classList.contains('hraciPole--kolecko')) {
     return 'circle';
+  }
+};
+
+const vyhrava = 5;
+const isWinningMove = (pole) => {
+  const origin = getPosition(pole);
+  const symbol = getSymbol(pole);
+
+  let i;
+
+  let vRadku = 1;
+  i = origin.column;
+  while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+    vRadku++;
+    i--;
+  }
+
+  i = origin.column;
+  while (
+    i < velikost - 1 &&
+    symbol === getSymbol(getField(origin.row, i + 1))
+  ) {
+    vRadku++;
+    i++;
+  }
+  if (vRadku >= vyhrava) {
+    return true;
+  }
+
+  let veSloupci = 1;
+  i = origin.row;
+  while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+    veSloupci++;
+    i--;
+  }
+
+  i = origin.row;
+  while (
+    i < velikost - 1 &&
+    symbol === getSymbol(getField(i + 1, origin.column))
+  ) {
+    veSloupci++;
+    i++;
+  }
+  if (veSloupci >= vyhrava) {
+    return true;
+  }
+  return false;
+};
+
+const vitezstvi = (pole) => {
+  if (isWinningMove(pole) === true) {
+    if (getSymbol(pole) === 'circle') {
+      window.confirm(
+        'Gratulujeme kolečku k vítězství! Chcete rozdrtit křížek ještě jednou?',
+      );
+      location.reload();
+    }
+  } else if (getSymbol(pole) === 'cross') {
+    window.confirm(
+      'Gratulujeme křížku k vítězství! Chcete rozdrtit kolečko ještě jednou?',
+    );
+    location.reload();
   }
 };
