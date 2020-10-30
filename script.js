25e7bfbd500e1abf2765e4ff8492a3c951868be2
Bara Maresova <bbmaresova@gmail.com>
2020-10-30 17:59:50 +0100

    přípravné funkce hotové

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let player = 'kolecko';
 const hraTlacitko = document.querySelector('.hraTlacitko');
 const activePlayer = document.querySelector('.activePlayer');
 
+const hraciPole = document.querySelectorAll('.hraTlacitko');
+
 const hra = (event) => {
   if (player === 'kolecko') {
     event.target.classList.add('hraciPole--kolecko');
@@ -21,10 +23,36 @@ const hra = (event) => {
     activePlayer.alt = 'Obrázek kolečka';
     player = 'kolecko';
   } else null;
+  console.log(getPosition(event.target));
+  console.log(getSymbol(event.target));
 };
 
-const hraciPole = document.querySelectorAll('.hraTlacitko');
 for (let i = 0; i < hraciPole.length; i += 1) {
   hraciPole[i].addEventListener('click', hra);
 }
 
+const velikost = 10;
+
+const getPosition = (pole) => {
+  let poleIndex = 0;
+  while (poleIndex < hraciPole.length) {
+    if (pole === hraciPole[poleIndex]) {
+      break;
+    }
+    poleIndex++;
+  }
+  return {
+    row: Math.floor(poleIndex / velikost),
+    column: poleIndex % velikost,
+  };
+};
+
+const getField = (row, column) => hraciPole[row * velikost + column];
+
+const getSymbol = (pole) => {
+  if (pole.classList.contains('hraciPole--krizek')) {
+    return 'cross';
+  } else if (pole.classList.contains('hraciPole--kolecko')) {
+    return 'circle';
+  }
+};
